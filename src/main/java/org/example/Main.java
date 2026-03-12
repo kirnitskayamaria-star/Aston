73adca3c5f84a30d02b0304e5dc064a4bb4bba68
mariakirnitskaya <maria105@mail.ru>
2026-03-12 23:02:20 +0700

    homework is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,12 @@ package org.example;
 import java.util.List;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
 
         ImmutableDepartment itDept = new ImmutableDepartment("IT", List.of("Иван", "Алексей", "Ольга"));
         ImmutableDepartment hrDept = new ImmutableDepartment("HR", List.of("Анна", "Михаил", "Мария"));
-
         itDept = itDept.addEmployee("Вячеслав");
         hrDept = hrDept.addEmployee("Екатерина");
-
         System.out.println(itDept);
         System.out.println(hrDept);
     }
