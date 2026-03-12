1b006cd51242e93584efdf34b29057f384c0c3b4
mariakirnitskaya <maria105@mail.ru>
2026-03-12 22:56:09 +0700

    homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,8 @@ public class Main {
 
         ImmutableDepartment itDept = new ImmutableDepartment("IT", List.of("Иван", "Алексей", "Ольга"));
         ImmutableDepartment hrDept = new ImmutableDepartment("HR", List.of("Анна", "Михаил", "Мария"));
-
         itDept = itDept.addEmployee("Вячеслав");
         hrDept = hrDept.addEmployee("Екатерина");
-
         System.out.println(itDept);
         System.out.println(hrDept);
     }
